6f70dae4ae96d0e2807c158a0f651846a9e8031f
SenthilBalaji <senthil.g.balaji@gmail.com>
2020-08-08 12:20:37 +0530

    Updates

---
+++ precache-manifest.54f0870cab8a41aa4f0a9e3fdf133859.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132a8f2399ac9924a83ff357fe3d3a32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa5ccc924e022cd920fd",
+    "url": "/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "9ac9f816eb1eff58fe5d",
+    "url": "/static/css/main.81acbc7b.chunk.css"
+  },
+  {
+    "revision": "aa5ccc924e022cd920fd",
+    "url": "/static/js/2.1bacab5a.chunk.js"
+  },
+  {
+    "revision": "c30bdf8d6e79bedc2de9f20f934a0680",
+    "url": "/static/js/2.1bacab5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac9f816eb1eff58fe5d",
+    "url": "/static/js/main.a356249d.chunk.js"
+  },
+  {
+    "revision": "083b4adc05a93d6a4b90",
+    "url": "/static/js/runtime-main.b9ac22f9.js"
+  }
+]);
